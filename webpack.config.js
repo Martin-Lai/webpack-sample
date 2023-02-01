8f1f48a7272387f36043add6d592de5dd34932d6
賴冠宇 <martin.lai.0314@gmail.com>
2023-02-01 16:04:00 +0800

    feat: typescript

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,20 @@ module.exports = {
     output: {
         filename: 'index.bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              // disable type checker - we will use it in fork plugin
+              transpileOnly: true
+            }
+          }
+        }
+      ]
     }
 };
