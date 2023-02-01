c4be086fce6b7547751fce131e1b6960843a6792
賴冠宇 <martin.lai.0314@gmail.com>
2023-02-01 16:10:58 +0800

    feat: add sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,46 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'index.bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'ts-loader',
+  entry: "./src/index.js",
+  output: {
+    filename: "index.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            // disable type checker - we will use it in fork plugin
+            transpileOnly: true,
+          },
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          process.env.NODE_ENV !== "production"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
             options: {
-              // disable type checker - we will use it in fork plugin
-              transpileOnly: true
-            }
-          }
-        }
-      ]
-    }
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
 };
